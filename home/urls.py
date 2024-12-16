a0b44b8fc2702a24870c392d9eb27648c042223b
Flavio-Lima18 <fplj2005@gmail.com>
2024-12-16 02:21:16 -0300

    Modicação para o banco de dados PostgreSql

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('categoria/lista', views.categoria, name="lista")
+    path('categoria/lista', views.categoria, name="lista"),
+    path('categoria/formulario', views.form_categoria, name="form_categoria"),
+    path('editar_categoria/<int:id>', views.editar_categoria, name="editar_categoria"),
+    path('detalhes_categoria/<int:id>', views.detalhes_categoria, name="detalhes_categoria"),
+    path('remover_categoria/<int:id>', views.remover_categoria, name="remover_categoria")
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Categoria
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .forms import *
+from .models import *
 
 def index(request):
     return render(request,'index.html')
@@ -10,3 +12,64 @@ def categoria(request):
    }
    return render(request, 'categoria/lista.html', contexto)
 
+def form_categoria(request, id=None):
+    if id:
+        try:
+            categoria = Categoria.objects.get(pk=id)  
+        except Categoria.DoesNotExist:
+            messages.error(request, 'Categoria não encontrada.')
+            return redirect('lista')
+    else:
+        categoria = None  
+
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST, instance=categoria) 
+        if form.is_valid():
+            form.save() 
+            messages.success(request, 'Operação realizada com sucesso.')
+            return redirect('lista')  
+        else:
+            messages.error(request, 'Erro ao processar o formulário.')
+    else:  
+        form = CategoriaForm(instance=categoria)  
+
+    return render(request, 'categoria/formulario.html', {'form': form})
+
+def editar_categoria(request, id):
+    try: 
+        categoria = Categoria.objects.get(pk=id)
+    except: 
+        messages.error(request, 'Registro não encontrado')
+        return redirect('lista')
+
+    if (request.method == 'POST'):
+        form = CategoriaForm(request.POST, instance = categoria)
+        if form.is_valid():
+            categoria = form.save()
+            lista=[]
+            lista.append(categoria)
+            return render(request, 'categoria/lista.html', {'lista':lista})    
+    else: 
+        form = CategoriaForm(instance = categoria)
+        return render(request, 'categoria/formulario.html', {'form': form})
+    
+def detalhes_categoria(request, id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        messages.error(request, 'Categoria não encontrada.')
+        return redirect('lista')
+    
+    return render(request, 'categoria/detalhes.html', {'categoria': categoria})
+
+
+def remover_categoria(request, id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+        categoria.delete()
+        messages.success(request, 'Exclusão realizada com Sucesso.')
+    except:
+        messages.error(request, 'Registro não encontrado')
+        return redirect('lista')
+    
+    return redirect('lista')
